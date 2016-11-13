e5509395e41462eb251836137948948d032a1106
kvr777 <kirill.v.romanov@gmail.com>
2016-11-13 21:06:26 +0300

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -4,13 +4,13 @@ data <- read.table(unz(temp, "household_power_consumption.txt"), sep=";", header
 unlink(temp)
 
 
-DATE1 <- as.Date("01/02/2007", format="%d/%m/%Y")
-DATE2 <- as.Date("02/02/2007",format="%d/%m/%Y")
+DATE1 <- as.POSIXct("01/02/2007", format="%d/%m/%Y")
+DATE2 <- as.POSIXct("03/02/2007",format="%d/%m/%Y")
 
 data$DateTime=paste(data$Date, data$Time)
 data$DateTime = as.POSIXlt(data$DateTime,format="%d/%m/%Y %H:%M:%S")
 
-mydata<-subset(data,(DateTime<=DATE2)&(DateTime>=DATE1))
+mydata<-subset(data,(DateTime<DATE2)&(DateTime>=DATE1))
 
 png(file="plot2.png", width = 480, height = 480, units = "px")
 plot(x=mydata$DateTime,y=mydata$Global_active_power,type = "l",xaxt="n", xlab=NA,ylab="Global Active Power (kilowatts)")
